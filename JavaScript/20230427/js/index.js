e39e4af57928753753b256a8a3ddab6fd8d1c82c
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-04-27 18:21:29 +0900

    :memo: 벤딩머신 이벤트 바인딩 장바구니 진행

--- JavaScript/20230427/js/index.js
+++ JavaScript/20230427/js/index.js
@@ -4,5 +4,16 @@ import VendingMachineEvents from "./classes/vendingMachineEvents.js";
 const colaGenerator = new  ColaGenerator();
 const vendingMachineEvents = new  VendingMachineEvents();
 
-colaGenerator.setup();
-vendingMachineEvents.bindEvent();
+// 탑레벨 await : 최상위 모듈에서 실행되는 await
+// 모듈중에서 index.js가 최상위에 있는데 
+// 최상위 모델에서는 async 없이 await으로 실행을 시켜도 상관이 없음
+
+
+//   (async function () {
+// })
+
+
+(async function () {
+    await colaGenerator.setup();
+    vendingMachineEvents.bindEvent();
+})()
