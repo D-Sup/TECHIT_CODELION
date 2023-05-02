0c0296f36dc55787b783da42996cff942c647db7
DongSup_Ahn <andongcelsius@gmail.com>
2023-05-02 17:18:46 +0900

    :memo: 바닐라 JS / 리액트 가상돔 / 리액트 JSX 문법 비교해보기

--- JavaScript/20230427/js/classes/vendingMachineEvents.js
+++ JavaScript/20230427/js/classes/vendingMachineEvents.js
@@ -1,119 +1,193 @@
 class VendingMachineEvents {
-  constructor() {
-      const vMachine = document.querySelector('.section1');
-      this.balance = vMachine.querySelector('.bg-box p');
-      this.itemList = vMachine.querySelector('.cola-list');
-      this.inputCostEl = vMachine.querySelector('#input-money');
-      this.btnPut = vMachine.querySelector('#input-money+.btn');
-      this.btnReturn = vMachine.querySelector('.bg-box+.btn');
-      this.btnGet = vMachine.querySelector('.btn-get');
-      this.stagedList = vMachine.querySelector('.get-list');
+constructor() {
+const vMachine = document.querySelector('.section1');
+this.balance = vMachine.querySelector('.bg-box p');
+this.itemList = vMachine.querySelector('.cola-list');
+this.inputCostEl = vMachine.querySelector('#input-money');
+this.btnPut = vMachine.querySelector('#input-money+.btn');
+this.btnReturn = vMachine.querySelector('.bg-box+.btn');
+this.btnGet = vMachine.querySelector('.btn-get');
+this.stagedList = vMachine.querySelector('.get-list');
 
-      const myinfo = document.querySelector('.section2');
-      this.myMoney = myinfo.querySelector('.bg-box strong');
+const myinfo = document.querySelector('.section2');
+this.myMoney = myinfo.querySelector('.bg-box strong');
 
-      const getInfo = document.querySelector('.section3');
-      this.getList = getInfo.querySelector('.get-list');
-      this.txtTotal = getInfo.querySelector('.total-price');
-  }
+const getInfo = document.querySelector('.section3');
+this.getList = getInfo.querySelector('.get-list');
+this.txtTotal = getInfo.querySelector('.total-price');
+}
 
-  //장바구니 콜라 생성 함수
-  stagedItemGenerator(target) {
-      const stagedItem = document.createElement('li');
+//장바구니 콜라 생성 함수
+stagedItemGenerator(target) {
+const stagedItem = document.createElement('li');
+stagedItem.dataset.item = target.dataset.item;
+stagedItem.dataset.price = target.dataset.price;
+stagedItem.innerHTML = `
+<img src="./img/${target.dataset.img}" alt="">
+${target.dataset.item}
+<strong>1
+<span class="a11y-hidden">개</span>
+</strong>
+`;
+this.stagedList.append(stagedItem);
+}
 
-      stagedItem.innerHTML = `
-      <img src="./img/${target.dataset.img}" alt="">
-          ${target.dataset.item}
-      <strong>1
-          <span class="a11y-hidden">개</span>
-      </strong>
-      `;
-      this.stagedList.append(stagedItem);
-  }
+bindEvent() {
+/**
+ * 1. 입금 버튼 기능
+ * 입금 버튼을 누르면
+ * 1) 소지금 === 소지금 - 입금액
+ * 2) 잔액 === 기존 잔액 + 입금액
+ * 3) 입금액이 소지금보다 많으면 경고창 출력
+ * 4) 입금액이 정상적으로 입금되면 입금창은 초기화 되야한다.
+ */
+this.btnPut.addEventListener('click', () => {
+    // 입력값
+    const inputCost = parseInt(this.inputCostEl.value);
+    // 소지금
+    const myMoneyVal = parseInt(this.myMoney.textContent.replaceAll(',', ''));
 
+    // 잔액
+    const balanceVal = parseInt(this.balance.textContent.replaceAll(',', ''));
 
-  bindEvent() {
-      /**
-       * 1. 입금 버튼 기능
-       * 입금 버튼을 누르면 
-       * 1) 소지금 === 소지금 - 입금액
-       * 2) 잔액 === 기존 잔액 + 입금액
-       * 3) 입금액이 소지금보다 많으면 경고창 출력
-       * 4) 입금액이 정상적으로 입금되면 입금창은 초기화 되야한다.
-       */
-      this.btnPut.addEventListener('click', () => {
-          // 입력값
-          const inputCost = parseInt(this.inputCostEl.value);
-          // 소지금 
-          const myMoneyVal = parseInt(this.myMoney.textContent.replaceAll(',', ''));
+    if (inputCost) {
+    // 입금액이 소지금 보다 적거나 같다면
+    if (inputCost <= myMoneyVal && inputCost > 0) {
+        this.myMoney.textContent = new Intl.NumberFormat().format(myMoneyVal - inputCost) + '원';
 
-          // 잔액
-          const balanceVal = parseInt(this.balance.textContent.replaceAll(',', ''));
+        this.balance.textContent = new Intl.NumberFormat().format((balanceVal ? balanceVal : 0) + inputCost) + '원';
 
-          if (inputCost) {
-              // 입금액이 소지금 보다 적거나 같다면
-              if (inputCost <= myMoneyVal && inputCost > 0) {
-                  this.myMoney.textContent = new Intl.NumberFormat().format(myMoneyVal - inputCost) + '원';
+        // 입금액이 소지금보다 많다!
+    } else {
+        alert('소지금이 부족합니다.');
+    }
+    // 입금액 초기화
+    this.inputCostEl.value = '';
+    }
+});
 
-                  this.balance.textContent = new Intl.NumberFormat().format((balanceVal ? balanceVal : 0) + inputCost) + '원';
+/**
+ * 2. 거스름돈 반환 버튼
+ * 1) 반환버튼을 누르면 소지금 === 잔액 + 소지금
+ * 2) 반환버튼을 누르면 잔액창이 초기화됩니다.
+ */
+this.btnReturn.addEventListener('click', () => {
+    // 잔액
+    const balanceVal = parseInt(this.balance.textContent.replaceAll(',', ''));
 
-                  // 입금액이 소지금보다 많다!
-              } else {
-                  alert('소지금이 부족합니다.');
-              }
-              // 입금액 초기화 
-              this.inputCostEl.value = "";
-          }
-      });
+    // 소지금
+    const myMoneyVal = parseInt(this.myMoney.textContent.replaceAll(',', ''));
 
+    if (balanceVal) {
+    this.myMoney.textContent = new Intl.NumberFormat().format(balanceVal + myMoneyVal) + '원';
+    this.balance.textContent = null;
+    }
+});
 
-      /**
-       * 2. 거스름돈 반환 버튼
-       * 1) 반환버튼을 누르면 소지금 === 잔액 + 소지금
-       * 2) 반환버튼을 누르면 잔액창이 초기화됩니다.
-       */
-      this.btnReturn.addEventListener('click', () => {
-          // 잔액
-          const balanceVal = parseInt(this.balance.textContent.replaceAll(',', ''));
+/**
+ * 3. 자판기 장바구니 채우기
+ * 1) 아이템을 누르면 잔액 === 잔액 - 아이템 가격
+ * 2) 아이템 가격이 잔액보다 크다면 경고메세지를 줍니다.
+ * 3) 아이템이 장바구니에 들어갑니다.
+ * 4) 아이템의 count가 -1이 됩니다. 아이템의 카운트가 0이되면 품절 표시를 합니다.
+ */
+this.btnsCola = document.querySelectorAll('.section1 .btn-cola');
 
-          // 소지금 
-          const myMoneyVal = parseInt(this.myMoney.textContent.replaceAll(',', ''));
+this.btnsCola.forEach(item => {
+    item.addEventListener('click', event => {
+    const balanceVal = parseInt(this.balance.textContent.replaceAll(',', ''));
+    const targetEl = event.currentTarget;
+    const targetElPrice = parseInt(targetEl.dataset.price);
+    const stagedListitem = this.stagedList.querySelectorAll('li')
+    let isStaged = false; // 이미 장바구니에 있는가?
 
-          if (balanceVal) {
-              this.myMoney.textContent = new Intl.NumberFormat().format(balanceVal + myMoneyVal) + '원';
-              this.balance.textContent = null;
-          }
-      });
+    if (balanceVal >= targetElPrice) {
+        this.balance.textContent = new Intl.NumberFormat().format(balanceVal - targetElPrice) + '원';
 
-      /**
-       * 3. 자판기 장바구니 채우기
-       * 1) 아이템을 누르면 잔액 === 잔액 - 아이템 가격
-       * 2) 아이템 가격이 잔액보다 크다면 경고메세지를 줍니다.
-       * 3) 아이템이 장바구니에 들어갑니다.
-       * 4) 아이템의 count가 -1이 됩니다. 아이템의 카운트가 0이되면 품절 표시를 합니다.
-       */
-      this.btnsCola = document.querySelectorAll('.section1 .btn-cola');
+        for (const item of stagedListitem) {
+            // 클릭한 콜라의 이름과 장바구니에 있던 콜라의 이름이 같은지 비교!
+            if(targetEl.dataset.item === item.dataset.item){
 
-      this.btnsCola.forEach((item) => {
-          item.addEventListener('click', (event) => {
-              const balanceVal = parseInt(this.balance.textContent.replaceAll(',', ''));
-              const targetElPrice = parseInt(event.currentTarget.dataset.price);
+                // 이미 장바구니에 콜라가 있다면 카운트 + 1
+                item.querySelector('strong').firstChild.textContent = parseInt(item.querySelector('strong').firstChild.textContent) + 1;
 
-              if (balanceVal >= targetElPrice) {
-                  this.balance.textContent = new Intl.NumberFormat().format(balanceVal - targetElPrice) + '원';
+                isStaged = true;
+                break;
+            }
+        }
+        // 처음 선택했을 경우에만 장바구니에 콜라를 생성합니다.
+        if(!isStaged) {
+            //장바구니 콜라 생성
+            this.stagedItemGenerator(event.currentTarget);
+        }
 
-                  //장바구니 콜라 생성
-                  this.stagedItemGenerator(event.currentTarget);
-                  // for (const item of this.stagedList) {
+        // 자판기 콜라 개수 차감
+        targetEl.dataset.count--;
 
-                  // }
+        if(!parseInt(targetEl.dataset.count)){
+            targetEl.insertAdjacentHTML('beforeEnd', `            
+                <strong class="soldout">
+                    <span>품절</span>
+                </strong>
+                `       
+            );
 
-              } else {
-                  alert('입금한 금액이 부족합니다.');
-              }
-          })
-      })
-  }
+            targetEl.disable = "disabled";
+        }
+
+    } else {
+        alert('입금한 금액이 부족합니다.');
+    }
+    });
+});
+
+/**
+ * 4. 획든 버튼 기능
+ * 1) 장바구니에 있는 음료수 목록이 획득한 음료 목록으로 이동합니다.
+ * 2) 획득한 음료의 모든 금액을 합하여 총 금액을 업데이트합니다.
+ */
+
+this.btnGet.addEventListener('click', () => {
+    const itemStagedList = this.stagedList.querySelectorAll('li');
+    const itemGetList = this.getList.querySelectorAll('li');
+    let totalPrice = 0;
+
+    // 쿼리로 잡으면 그대론데
+    // 자식으로 잡으면 하나씩 사라져요?
+    // const itemStagedList = this.stagedList.children 
+    // this.getList.append(...itemStaged);
+    for(const itemStaged of itemStagedList){
+        let isGet = false; // 이미 획득했는가?
+        for(const itemGet of itemGetList){
+            // 장바구니의 콜라가 이미 획득한 목록에 있다면
+            if(itemStaged.dataset.item === itemGet.dataset.item) {
+                // 이미 장바구니에 콜라가 있다면 카운트 + 1
+                itemGet.querySelector('strong').firstChild.textContent = parseInt(itemGet.querySelector('strong').firstChild.textContent) + parseInt(itemStaged.querySelector('strong').firstChild.textContent);
+
+                isGet = true;
+                break;
+            }
+        }
+        if(!isGet){
+            this.getList.append(itemStaged);
+        }
+    }
+
+    // 장바구니 목록 초기화
+    this.stagedList.innerHTML = null;
+
+    // 획득한 음료 리스트를 순회하면서 총금액을 계산합니다
+    this.getList.querySelectorAll('li').forEach((itemGet) => {
+        // this.txtTotal.textContent = parseInt(this.txtTotal.textContent) + parseInt(itemGet.dataset.price) * parseInt(itemGet.querySelector('strong').firstChild.textContent);
+        totalPrice += parseInt(itemGet.dataset.price) * parseInt(itemGet.querySelector('strong').firstChild.textContent);
+        this.txtTotal.textContent = `총금액 : ${new Intl.NumberFormat().format(totalPrice)} 원`
+    })
+});
+
+}
 }
 
 export default VendingMachineEvents;
+
+// 반복되는 parseInt 나 NumberFormat 은 따로 함수를 만들어보기
+// 이코드를 리액트로 바꿔보는 것도
